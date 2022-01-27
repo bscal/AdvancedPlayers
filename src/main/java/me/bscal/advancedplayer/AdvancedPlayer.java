ad5d8858b56dc0e2207847193f3ac08ea6e1f62b
bscal <jonfus123@hotmail.com>
2022-01-26 19:43:04 -0500

    initial commit

---
+++ src/main/java/me/bscal/advancedplayer/AdvancedPlayer.java
@@ -0,0 +1,15 @@
+package me.bscal.advancedplayer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AdvancedPlayer implements ModInitializer
+{
+
+	public static final String MOD_ID = "advancedplayer";
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/me/bscal/advancedplayer/client/AdvancedPlayerClient.java
@@ -0,0 +1,14 @@
+package me.bscal.advancedplayer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT) public class AdvancedPlayerClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+
+	}
+}
